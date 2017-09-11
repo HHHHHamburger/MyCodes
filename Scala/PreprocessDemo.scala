209500d0d7d5f1e2c18ab54ed0c646a8974c93ba
Milafa <isolatedzebra@hotmail.com>
2017-09-11 12:04:55 +0800

    Add files via upload

---
+++ Scala/PreprocessDemo.scala
@@ -0,0 +1,27 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import preprocess.Preprocessor
+
+/**
+  * 预处理，包括: "数据清洗", "标签索引化", "分词", "向量化"
+  *
+  */
+object PreprocessDemo extends Serializable {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.WARN)
+    //    HanLP.Config.enableDebug()
+
+    val spark = SparkSession
+      .builder
+      .master("local[2]")
+      .appName("Preprocess Demo")
+      .getOrCreate()
+
+    val filePath = "data/tmp"
+
+    val preprocessor = new Preprocessor
+    preprocessor.train(filePath, spark)
+
+    spark.stop()
+  }
+}
